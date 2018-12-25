434c788312cdc41bd8e4043ca239a06a04d7ffdd
ShanjianII <twfxtianweifeixian@gmail.com>
2018-12-25 22:17:57 +0000

    fvv

--- shoppingbackend/src/main/java/net/cls/shoppingbackend/dao/CategoryDAO.java
+++ shoppingbackend/src/main/java/net/cls/shoppingbackend/dao/CategoryDAO.java
@@ -1,5 +1,14 @@
 package net.cls.shoppingbackend.dao;
 
+import java.util.List;
+
+import net.cls.shoppingbackend.dto.Category;
+
 public interface CategoryDAO {
 
+	
+	
+	List<Category> list();
+	
+	
 }
